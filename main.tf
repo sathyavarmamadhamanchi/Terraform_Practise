a997fc165b8c4ed6566a089db0dea01fd0e68926
sathyavarmamadhamanchi <144237296+sathyavarmamadhamanchi@users.noreply.github.com>
2023-11-14 04:53:44 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -58,7 +58,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "state_encryption"
 
 data "aws_iam_policy_document" "mypolicydocument" {
   statement {
-    sid = "allow_s3"
+    sid = "allows3"
     effect = "Allow"
     actions = [  
       "s3:GetObject",
