1c24e3e998f3ba5a74571a2e68fe01753ea0ce76
sathyavarmamadhamanchi <144237296+sathyavarmamadhamanchi@users.noreply.github.com>
2023-11-16 04:44:26 -0500

    Create ecr.tf

---
+++ ecr.tf
@@ -0,0 +1,27 @@
+module "ecr" {
+  source = "terraform-aws-modules/ecr/aws"
+
+  repository_name = "Practise-repo"
+  repository_lambda_read_access_arns = "arn:aws:iam::615238735504:user/my_user"
+  repository_lifecycle_policy = jsondecode({
+   rules = [
+      {
+        rulePriority = 1,
+        description  = "Keep last 30 images",
+        selection = {
+          tagStatus     = "tagged",
+          tagPrefixList = ["v"],
+          countType     = "imageCountMoreThan",
+          countNumber   = 30
+        },
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+})
+tags = {
+    Terraform = "true"
+    Environment = "Dev"
+}
+}
