c53a08893a4af1cd346f7807d202c3a92a83e9df
sathyavarmamadhamanchi <144237296+sathyavarmamadhamanchi@users.noreply.github.com>
2023-11-16 04:21:39 -0500

    Create dynamodb.tf

---
+++ dynamodb.tf
@@ -0,0 +1,22 @@
+module "dynamodb_table" {
+  source = "terraform-aws-modules/dynamodb-table/aws"
+
+  name = "my-table"
+  hash_key = "id"
+
+  attributes = [
+    {
+        name = "id"
+        type = "N"
+    }
+    {
+        name = "Sname"
+        type = "S"
+    }
+  ]
+
+  tags = {
+    Terraform = "true"
+    Enivironment = "staging"
+  }
+}
