a0df4aa4f804c0485ea650121052160978b8d846
sathyavarmamadhamanchi <144237296+sathyavarmamadhamanchi@users.noreply.github.com>
2023-11-16 04:53:44 -0500

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,19 @@
+module "vpc" {
+  
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "practise-vpc"
+  cidr = "10.1.0.0/16"
+
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  private_subnets = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24"]
+  public_subnets  = ["10.1.101.0/24", "10.1.102.0/24", "10.1.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
